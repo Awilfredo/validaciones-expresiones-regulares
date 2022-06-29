ab6073480e1935f58e42ffe0ac639bd3780e336f
Wilfredo Cruz <davidernestotores@gmail.com>
2022-06-28 22:45:43 -0500

    validacion con toggle reduce codigo

--- js/code.js
+++ js/code.js
@@ -3,48 +3,22 @@ function validarDatos(){
     const lastName= document.getElementById('apellido');
     const email= document.getElementById('email');
     const pass= document.getElementById('password');
+    const advertencia= document.getElementById('pass-req')
     var isValid=true;
-
-
     var expRegNombres=new RegExp("^([a-zA-z]{4,12}[\\s]?){1,2}$");
-    
     var expRegCorreo= new RegExp("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$");
-
     var expRegpass= new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,20}$");
 
-    if(!expRegNombres.test(name.value)){
-        name.className="form-control is-invalid";
-        isValid=false;
-    } else{
-        name.className="form-control";
-    }
+    name.classList.toggle('is-invalid', !expRegNombres.test(name.value));
+    lastName.classList.toggle('is-invalid', !expRegNombres.test(lastName.value));
+    email.classList.toggle('is-invalid', !expRegCorreo.test(lastName.value));
 
-    if(!expRegNombres.test(lastName.value)){
-        lastName.className="form-control is-invalid";
-        isValid=false;
-    }else{
-        lastName.className="form-control";
-    }
+    isValid=expRegpass.test(pass.value);
+    pass.classList.toggle('is-invalid', !isValid);
+    advertencia.classList.toggle('d-none', isValid)
 
-    if (!expRegCorreo.test(email.value)) {
-        email.className="form-control is-invalid";  
-        isValid=false;              
-    } else {
-        email.className="form-control";
-    }
-
-    if(isValid){
-        if (!expRegpass.test(pass.value)) {
+        if (!isValid) {
             pass.focus();
             pass.value="";
-            pass.className="form-control is-invalid";
-            alert("La Contraseña debe tener:\nMinimo 8 caracteres\nMaximo 15\nAl menos una letra mayúscula\nAl menos una letra minucula\nAl menos un dígito\nNo espacios en blanco\nAl menos 1 caracter especial");
-                    
-        } else {
-            pass.className="form-control";
-                     
         }
     }
-
-
-}
